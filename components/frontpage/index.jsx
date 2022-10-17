8e1edec0b0193404f469f27af7b69dc4945253f1
Phon1209 <ptphon@gmail.com>
2022-10-17 10:53:20 -0300

    Added Motion to the title

--- components/frontpage/index.jsx
+++ components/frontpage/index.jsx
@@ -1,13 +1,55 @@
 import React from "react";
 import BubbleImg from "./bubble/bubble_img";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: [0.645, 0.045, 0.355, 1],
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const item = {
+  hidden: {
+    opacity: 0,
+    x: -10,
+  },
+  show: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      stiffness: 500,
+    },
+  },
+};
 
 const Frontpage = () => {
   return (
     <div className="h-full w-full flex flex-col lg:flex-row">
-      <section className=" text-white text-center p-16 flex justify-center items-center flex-col min-h-[400px] lg:items-start lg:text-left">
-        <h1 className="text-display-lg">Patiphon Loetsuthakun</h1>
-        <h3 className="text-headline-sm">Frontend Developer</h3>
-      </section>
+      <motion.section
+        variants={container}
+        initial="hidden"
+        whileInView="show"
+        className=" text-white text-center p-16 flex justify-center items-center flex-col min-h-[400px] lg:pl-24 lg:items-start lg:text-left"
+      >
+        <motion.h1
+          variants={item}
+          layoutId="name"
+          animate
+          className="text-display-lg"
+        >
+          Patiphon Loetsuthakun
+        </motion.h1>
+        <motion.h3 variants={item} animate className="text-headline-sm">
+          Frontend Developer
+        </motion.h3>
+      </motion.section>
       <section className="flex-grow flex justify-center items-center">
         <BubbleImg size={300} onPage={false} />
       </section>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
+import { motion } from "framer-motion";
 import Frontpage from "../components/frontpage";
 import Bubble from "../components/frontpage/bubble/bubble";
+import Project from "../components/project";
 
 export default function Home() {
   return (
@@ -13,19 +15,20 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="fullpage-item">
-        <Bubble size={57} x={73} y={42}  />
-        <Bubble size={57} x={100} y={600}  />
-        <Bubble size={156} x={66} y={760}  />
-        <Bubble size={29} x={300} y={430}  />
-        <Bubble size={45} x={500} y={130}  />
-        <Bubble size={100} x={400} y={880}  />
-        <Bubble size={57} x={270} y={1100}  />
+
+      <motion.section layout className="fullpage-item">
+        <Bubble size={57} x={73} y={42} />
+        <Bubble size={57} x={100} y={600} />
+        <Bubble size={156} x={66} y={760} />
+        <Bubble size={29} x={300} y={430} />
+        <Bubble size={45} x={500} y={130} />
+        <Bubble size={100} x={400} y={880} />
+        <Bubble size={57} x={270} y={1100} />
         <Frontpage />
-      </section>
-      <section className="fullpage-item">
-        <h1 className="text-2xl text-dark-white-extra">Hello World</h1>
-      </section>
+      </motion.section>
+      <motion.section layout className="fullpage-item">
+        <Project />
+      </motion.section>
     </>
   );
 }
