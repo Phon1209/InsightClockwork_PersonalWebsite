b2f20841c13bb593bfeaa7919431e8d7641b8874
Phon1209 <ptphon@gmail.com>
2023-02-12 00:20:26 -0400

    Bubble Physic

--- components/bubble/bubbleText.tsx
+++ components/bubble/bubbleText.tsx
@@ -1,16 +1,54 @@
 import React from "react";
-import bubbleSrc from "../../public/imgs/bubbleImg.png";
+import bubbleSrc1 from "../../public/imgs/bubble1.png";
+import bubbleSrc2 from "../../public/imgs/bubble2.png";
+import bubbleSrc3 from "../../public/imgs/bubble3.png";
+import { position } from "../frontpage/bubble";
+import { StaticImageData } from "next/image";
 
-const BubbleText = ({ text }: { text: string }) => {
+const BubbleText = ({
+  text,
+  size,
+  x,
+  y,
+  rotate,
+}: {
+  text: string;
+  size: number;
+  x: number;
+  y: number;
+  rotate?: number;
+}) => {
+  const bubbleImgSrcs: Array<StaticImageData> = [
+    bubbleSrc1,
+    bubbleSrc2,
+    bubbleSrc3,
+  ];
+
+  const rotateAngle = rotate || 0;
+
   return (
     <div
       style={{
-        backgroundImage: `url(${bubbleSrc.src})`,
+        backgroundImage: `url(${bubbleSrc1.src})`,
         aspectRatio: "1/1",
+        transitionProperty: "top left",
+        transitionDuration: "1000ms",
+        transitionTimingFunction: "ease",
+        top: y,
+        left: x,
+        translate: "-50% -50%",
+        scale: `${size}`,
+        rotate: `${rotateAngle}deg`,
       }}
-      className="text-title-lg bg-contain p-6 inline-flex justify-center items-center"
+      className="text-title-lg absolute bg-contain p-6 lg:p-8 inline-flex justify-center items-center"
     >
-      {text}
+      <h3
+        style={{
+          rotate: `${-rotateAngle}deg`,
+        }}
+      >
+        {text}
+      </h3>
     </div>
   );
 };
--- components/frontpage/index.jsx
+++ components/frontpage/index.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { MouseEventHandler } from "react";
 import Link from "next/link";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
 import { IoMdMail } from "react-icons/io";
 import { LinkIcon } from "../../types/links";
 import Skills from "./skills";
 import Links from "./links";
-import BubbleText from "../bubble/bubbleText";
+import BubbleSection from "./bubble";
+import { IconType } from "react-icons/lib";
 
-const contactLists: LinkIcon[] = [
+const contactLists = [
   {
     icon: BsLinkedin,
     title: "LinkedIn",
@@ -25,11 +26,15 @@ const contactLists: LinkIcon[] = [
   },
 ];
 
+const IconButton = React.forwardRef((props, ref) => {
+  return <a href={props.href} onClick={props.onClick} ref={ref}></a>;
+});
+
 const Frontpage = () => {
   return (
     <>
       <div className="py-12 px-8 text-white flex flex-col min-h-full md:grid md:grid-cols-12">
-        <section className="md:col-span-5 md:col-start-1">
+        <section className="md:col-span-5 md:col-start-1 md:flex md:flex-col md:items-start md:my-auto lg:ml-4">
           <h1 className="text-display-md lg:text-display-lg">
             Patiphon Loetsuthakun
           </h1>
@@ -37,7 +42,7 @@ const Frontpage = () => {
           <ul className="">
             {contactLists.map((elem, i) => {
               return (
-                <Link key={elem.link} href={elem.link}>
+                <Link key={elem.link} href={elem.link} passHref>
                   <elem.icon
                     className={`inline box-content w-6 h-6 ${
                       i != 0 ? "mx-2" : "mr-2"
@@ -52,12 +57,7 @@ const Frontpage = () => {
         <Links />
         <Skills />
         {/* Bubble */}
-        <section className="hidden md:block md:col-end-13 md:col-span-7">
-          <BubbleText text="Hello" />
-          <BubbleText text="Projects" />
-          <BubbleText text="Awards" />
-          <BubbleText text="About" />
-        </section>
+        <BubbleSection />
       </div>
     </>
   );
