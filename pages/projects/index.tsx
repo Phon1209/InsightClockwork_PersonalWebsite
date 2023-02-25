3680d2da789296cbd3e9a017ac77b484a865d2eb
Phon1209 <ptphon@gmail.com>
2023-02-24 21:43:55 -0400

    Added background for project card

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -3,6 +3,37 @@ import Head from "next/head";
 import { MdChevronLeft } from "react-icons/md";
 import projects from "../../public/json/projects.json";
 import { convertDate } from "../../utilities/Date";
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiIbmwatson,
+  SiReact,
+  SiArduino,
+  SiFirebase,
+  SiExpress,
+  SiHtml5,
+  SiCss3,
+  SiJquery,
+  SiMicrosoftazure,
+  SiPhpmyadmin,
+  SiJavascript,
+} from "react-icons/si";
+
+const icons = {
+  SiNextdotjs,
+  SiFramer,
+  SiIbmwatson,
+  SiReact,
+  SiArduino,
+  SiFirebase,
+  SiExpress,
+  SiHtml5,
+  SiCss3,
+  SiJquery,
+  SiMicrosoftazure,
+  SiPhpmyadmin,
+  SiJavascript,
+};
 
 const Projects: FC = () => {
   return (
@@ -11,26 +42,38 @@ const Projects: FC = () => {
         <title>Projects | Patiphon Loetsuthakun</title>
       </Head>
       <section className="fullpage-item px-12 py-16">
-        <header className="flex items-center">
+        <header className="flex items-center mb-8 md:mb-16">
           <MdChevronLeft className="w-12 h-12" />
           <h2 className="text-display-md">Project</h2>
         </header>
-
-        {projects.map((project) => {
-          return (
-            <article>
-              <header>
-                <h4>{project.name}</h4>
-                <p>{convertDate(project.startDate, project.endDate)}</p>
-              </header>
-              <summary>
-                {project.techs.map((icon) => {
-                  return <div>{icon}</div>;
-                })}
-              </summary>
-            </article>
-          );
-        })}
+        <div className="grid grid-flow-col auto-cols-auto">
+          {projects.map((project) => {
+            return (
+              <article
+                key={project.name}
+                style={{
+                  background: project.background,
+                }}
+                className="flex flex-col gap-6 items-center w-fit h-[210px] p-2 min-w-[240px] rounded-[30px] bg-black justify-center"
+              >
+                <header className="flex flex-col gap-2 items-center">
+                  <h4 className="text-headline-md font-semibold">
+                    {project.name}
+                  </h4>
+                  <p className="text-title-sm">
+                    {convertDate(project.startDate, project.endDate)}
+                  </p>
+                </header>
+                <summary className="list-none flex gap-4">
+                  {project.techs.map((iconName) => {
+                    const IconComponent = icons[iconName];
+                    return <IconComponent key={iconName} className="w-6 h-6" />;
+                  })}
+                </summary>
+              </article>
+            );
+          })}
+        </div>
       </section>
     </>
   );
