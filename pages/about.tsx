e3269aa7b9229cedbd10471dc3449300b372e3c2
Patiphon Loetsuthakun <36232545+Phon1209@users.noreply.github.com>
2023-03-11 15:30:52 -0500

    Added the reason in about apge

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,37 +11,44 @@ const About = () => {
       <Head>
         <title>About | Patiphon Loetsuthakun</title>
       </Head>
-      <section className="fullpage-item px-4 md:px-12 py-8 md:py-16">
+      <section className="fullpage-item fullpage-item--scrollable scrollbar-hide px-4 md:px-12 py-8 md:py-16 flex flex-col gap-12">
         <Link href="/">
-          <header className="flex items-center mb-8 md:mb-16 cursor-pointer self-start">
+          <header className="flex items-center md:mb-4 cursor-pointer self-start">
             <MdChevronLeft className="w-8 h-8" />
             <h2 className="text-title-lg md:text-display-sm text-code">
               About
             </h2>
           </header>
         </Link>
-        <article className="flex p-6 px-8 flex-col-reverse gap-12 md:flex-row md:px-20">
+        <article className="flex p-6 px-8 flex-col-reverse gap-8 md:gap-12 md:flex-row md:px-20">
           <div className="flex flex-col flex-1 self-center">
-            <header className="hidden md:block !font-semibold text-display-sm text-code">
+            <header className="hidden md:block !font-semibold text-display-sm text-code mb-4">
               Me?
             </header>
-            <summary className="list-none">{aboutData.intro}</summary>
+            <summary className="list-none text-title-lg tracking-wide">
+              {aboutData.intro}
+            </summary>
           </div>
-          <div className="lg:mx-20 xl:grow xl:mx-0 flex gap-12 justify-evenly items-center">
+          <div className="lg:mx-20 xl:grow xl:mx-0 flex gap-12 justify-evenly items-center scale-[0.8] md:scale-100">
             <Image
               objectFit="contain"
               width={200}
               height={200}
-              src={`/imgs/bubble1.png`}
+              src={`/imgs/bubble_profile.png`}
             />
             <header className="md:hidden !font-semibold text-display-sm text-code">
               Me?
             </header>
           </div>
         </article>
-        <article>{aboutData.goal}</article>
-        <article>{aboutData.why}</article>
-        <article>{aboutData.hobby}</article>
+        <article className="flex flex-col gap-16 p-6 px-8">
+          <header className="ml-6 md:ml-24">{aboutData.goal_transition}</header>
+          <summary className="list-none text-right self-end w-full sm:w-2/3">
+            {aboutData.goal}
+          </summary>
+        </article>
+        <article className="p-6 px-8">{aboutData.why}</article>
+        <article className="p-6 px-8">{aboutData.hobby}</article>
       </section>
     </>
   );
